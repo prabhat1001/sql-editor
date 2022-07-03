4109edbb770f542fc7cfbed505d693310d0ddd09
prabhat1001 <prabhatsaraswat24@gmail.com>
2022-07-03 18:18:59 +0530

    update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import Header from "./components/Header";
 import Home from "./Pages/Home";
 import About from "./Pages/About";
 import Help from "./Pages/Help";
+import Table1 from "./Pages/Table1";
+import Table2 from "./Pages/Table2";
+import Table3 from "./Pages/Table3";
+import Schema from "./Pages/Schema";
 import './App.css';
 
 
@@ -16,10 +20,15 @@ function App() {
         <Routes> 
           <Route path="/" exact element={<Login />}></Route>
           <Route path ='/about' element={<About/>}></Route> 
-          <Route path ="/home" element={<Home/>}></Route> 
+          <Route path ='/home' element={<Home/>}></Route> 
           <Route path ='/help' element={<Help/>}></Route>
+          <Route path ='/schema' element={<Schema/>}></Route>
+          <Route path ='schema/table1' element={<Table1/>}></Route>
+          <Route path ='schema/table2' element={<Table2/>}></Route>
+          <Route path ='schema/table3' element={<Table3/>}></Route>
           
         </Routes>
+        
       </Router>
     </div>
   );
